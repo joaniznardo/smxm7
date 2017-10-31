7e85f8d928762b6071becac7aff74cf49a8bab41
joaniznardo <joan.iznardo@iesjoandaustria.org>
2017-10-31 15:52:34 +0100

    error lab18 > lab20

--- uf1-dhcp-dns/labs/lab20/recull_lab20.sh
+++ uf1-dhcp-dns/labs/lab20/recull_lab20.sh
@@ -6,6 +6,6 @@
 ##
 
 # efectuem la "triangulació de pings..." per comprovar
-vagrant ssh lab18vm1 -c "ping -c 3 lab20vm2 "
-vagrant ssh lab18vm2 -c "ping -c 3 lab20vm3 "
-vagrant ssh lab18vm3 -c "ping -c 3 lab20vm1 "
+vagrant ssh lab20vm1 -c "ping -c 3 lab20vm2 "
+vagrant ssh lab20vm2 -c "ping -c 3 lab20vm3 "
+vagrant ssh lab20vm3 -c "ping -c 3 lab20vm1 "
